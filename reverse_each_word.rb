f021df10cb2a8c2f228fdc0696005ae2576146e5
Andrew Silva <andrewsilva9@gmail.com>
2019-04-25 00:21:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,9 @@
 
 def reverse_each_word(string)
   string = "Hello there, and how are you?".split(/ /)
+  new_array = []
   string.each do |word|
-    word.reverse
+    new_array << word.reverse
   end
   string.join(" ")
 end
